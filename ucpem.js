bd688e7692cfc159361963d90eb7b46f8caf191d
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-04-20 18:25:53 +0200

    Fixed UCPeM config

--- ucpem.js
+++ ucpem.js
@@ -2,6 +2,6 @@
 
 const { project, github } = require("ucpem")
 
-project.prefix("src").res("app")
+project.prefix("src").res("dependencyInjection")
 
 project.prefix("test").use(github("bt7s7k7/TestUtil").res("testUtil"))
