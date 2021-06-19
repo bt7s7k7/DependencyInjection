197c56dacfd83e6842211396216908d25b52449f
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-06-19 18:40:33 +0200

    AwaitInject component for vue

---
+++ src/dependencyInjectionVue/AwaitInject.tsx
@@ -0,0 +1,24 @@
+import { defineComponent, PropType } from "vue"
+import { DIService } from "../dependencyInjection/DIService"
+import { useContext } from "./hooks"
+
+export const AwaitInject = (defineComponent({
+    name: "AwaitInject",
+    props: {
+        def: {
+            type: null as unknown as PropType<DIService.ServiceDefinition>,
+            required: true
+        }
+    },
+    setup(props, ctx) {
+        const context = useContext()
+
+        const state = context.tryInject(props.def)
+
+        return () => (
+            <div>
+                {ctx.slots[state.value ? "default" : "pending"]?.()}
+            </div>
+        )
+    }
+}))
